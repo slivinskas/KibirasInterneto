b1ee316f7a1b01472a1e0a6eabad2842a7b31ed6
Vytenis Slivinskas <slivinskasvytenis@gmail.com>
2015-08-15 15:23:12 +0300

    šablono atnaujinimai

--- Web/js/web.js
+++ Web/js/web.js
@@ -1,8 +1,7 @@
 function initPano() {
   // Note: constructed panorama objects have visible: true
   // set by default.
-  var panorama = new google.maps.StreetViewPanorama(
-      document.getElementById('map'), {
+  var mapSetings = {
         position: {lat: 42.345573, lng: -71.098326},
         addressControlOptions: {
           position: google.maps.ControlPosition.BOTTOM_CENTER
@@ -13,7 +12,12 @@ function initPano() {
           style: google.maps.ZoomControlStyle.SMALL
         },
         enableCloseButton: false
-  });
+  };
+  var panorama = new google.maps.StreetViewPanorama(
+      document.getElementById('map1'), mapSetings);
+	  panorama = new google.maps.StreetViewPanorama(
+      document.getElementById('map2'), mapSetings);
+	  
 }
 
 initPano();
