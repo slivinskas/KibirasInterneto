e45953d84612a341d10824b4ac9377ffc670b82d
Vytenis Slivinskas <slivinskasvytenis@gmail.com>
2015-08-15 17:18:35 +0300

    padariau globalų size

--- Web/js/webMapM.js
+++ Web/js/webMapM.js
@@ -1,17 +1,13 @@
-
-var size = {
+size = {
 	width : 0
 	height : 0
 	update : function () {
 		this.width = $( document ).width()/2;
 		this.height = $( document ).height()/2;
-		$( "map1").height();
+		$( "map1").height() = ;
 		$( "map2").height();
 		$( "map1").width();
 		$( "map2").width();
 	}
 }
 
-var web = {
-
-}
