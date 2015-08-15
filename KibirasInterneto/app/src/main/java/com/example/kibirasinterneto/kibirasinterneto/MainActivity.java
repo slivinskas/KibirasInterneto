a9efe9623e369a5c100d567db9e3c4d2baa9334b
Tomas Jačionis <jacionis.tomas@gmail.com>
2015-08-15 15:49:10 +0300

    Full screen

--- KibirasInterneto/app/src/main/java/com/example/kibirasinterneto/kibirasinterneto/MainActivity.java
+++ KibirasInterneto/app/src/main/java/com/example/kibirasinterneto/kibirasinterneto/MainActivity.java
@@ -4,12 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
     }
 
