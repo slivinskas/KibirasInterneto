d2bfc5bdbba4c7d8fb62608474c2444de78d7195
Vytenis Slivinskas <slivinskasvytenis@gmail.com>
2015-08-15 16:43:07 +0300

    Daromas vertimas

--- Web/js/webMapM.js
+++ Web/js/webMapM.js
@@ -1,3 +1,17 @@
+
+var size = {
+	width : 0
+	height : 0
+	update : function () {
+		this.width = $( document ).width()/2;
+		this.height = $( document ).height()/2;
+		$( "map1").height();
+		$( "map2").height();
+		$( "map1").width();
+		$( "map2").width();
+	}
+}
+
 var web = {
-	
+
 }
