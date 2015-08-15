7f32190364e22a41cc7bb66a4a9991d7e36609bc
Vytenis Slivinskas <slivinskasvytenis@gmail.com>
2015-08-15 14:54:18 +0300

    Atnaujinta

--- Web/js/web.js
+++ Web/js/web.js
@@ -1,4 +1,23 @@
-var web = {
+function initPano() {
+  // Note: constructed panorama objects have visible: true
+  // set by default.
+  var panorama = new google.maps.StreetViewPanorama(
+      document.getElementById('map'), {
+        position: {lat: 42.345573, lng: -71.098326},
+        addressControlOptions: {
+          position: google.maps.ControlPosition.BOTTOM_CENTER
+        },
+        linksControl: false,
+        panControl: false,
+        zoomControlOptions: {
+          style: google.maps.ZoomControlStyle.SMALL
+        },
+        enableCloseButton: false
+  });
+}
 
+initPano();
+var web = {
+	
 
 }
