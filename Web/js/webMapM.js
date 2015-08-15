304e65c16cdf60835c6c0b97170b99bf734cefb0
Vytenis Slivinskas <slivinskasvytenis@gmail.com>
2015-08-15 20:53:46 +0300

    pridedu id

--- Web/js/webMapM.js
+++ Web/js/webMapM.js
@@ -1,13 +1,14 @@
 var size = {
+	that : this,
 	width : 0,
 	height : 0,
 	update : function () {
-		this.width = $( document ).width()/2;
-		this.height = $( document ).height()/2;
-		$( "map1").height(this.width);
-		$( "map2").height(this.height);
-		$( "map1").width(this.width);
-		$( "map2").width(this.height);
+		/*that.width = $( document ).width()/2;
+		that.height = $( document ).height()/2;
+		$( "#map1").height(that.width);
+		$( "#map2").height(that.height);
+		$( "#map1").width(that.width);
+		$( "#map2").width(that.height);*/
 	}
 }
 
