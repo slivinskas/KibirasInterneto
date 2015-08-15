1548ffefd2f686c2729b05a18f2f952c195da54f
Alius Petraška <alius.petraska@adform.com>
2015-08-15 20:09:33 +0300

    project cleanup;

--- KibirasInterneto/app/src/main/java/com/example/kibirasinterneto/kibirasinterneto/MainActivity.java
+++ KibirasInterneto/app/src/main/java/com/example/kibirasinterneto/kibirasinterneto/MainActivity.java
@@ -1,23 +1,14 @@
 package com.example.kibirasinterneto.kibirasinterneto;
 
-
-import android.app.Service;
 import android.content.Context;
-import android.content.Intent;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 import android.view.WindowManager;
 import android.webkit.WebView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,17 +20,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         String url = "http://kibirasinterneto.azurewebsites.net";
+
         WebView view = (WebView) this.findViewById(R.id.webView);
         view.getSettings().setJavaScriptEnabled(true);
         view.loadUrl(url);
 
-
         LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
         LocationListener locationListener = new LocationListener() {
             @Override
             public void onStatusChanged(String provider, int status, Bundle extras) {
@@ -66,8 +57,8 @@ public class MainActivity extends AppCompatActivity {
                 oldLocation = location;
             }
         };
-        locationManager.requestLocationUpdates(getProviderName(), 1000,
-                1, locationListener);
+
+        locationManager.requestLocationUpdates(getProviderName(), 1000, 1, locationListener);
     }
 
     String getProviderName() {
