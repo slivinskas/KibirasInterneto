5be3f27a5c3a90fbd479587a1cac6a039bff69ad
vilimas <vilius.pad@gmail.com>
2015-08-15 18:17:01 +0300

    Location Task

--- KibirasInterneto/app/src/main/java/com/example/kibirasinterneto/kibirasinterneto/MainActivity.java
+++ KibirasInterneto/app/src/main/java/com/example/kibirasinterneto/kibirasinterneto/MainActivity.java
@@ -1,17 +1,81 @@
 package com.example.kibirasinterneto.kibirasinterneto;
 
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Location oldLocation;
+    double diffLatitude = 0;
+    double diffLongitude = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+                Toast.makeText(MainActivity.this,
+                        "Provider enabled: " + provider, Toast.LENGTH_SHORT)
+                        .show();
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Toast.makeText(MainActivity.this,
+                        "Provider disabled: " + provider, Toast.LENGTH_SHORT)
+                        .show();
+            }
+
+            @Override
+            public void onLocationChanged(Location location) {
+               if(oldLocation == null){
+                   oldLocation = location;
+                   return;
+
+               }
+                diffLatitude = oldLocation.getLatitude() - location.getLatitude();
+                diffLongitude = oldLocation.getLongitude() - location.getLongitude();
+                oldLocation = location;
+                TextView txt = (TextView) findViewById(R.id.textView);
+                txt.setText("GPS: " + diffLatitude + " x " + diffLongitude);
+            }
+        };
+        locationManager.requestLocationUpdates(getProviderName(), 1000,
+                1, locationListener);
     }
+
+    String getProviderName() {
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        criteria.setPowerRequirement(Criteria.ACCURACY_HIGH);
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setSpeedRequired(true);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+        criteria.setCostAllowed(false);
+        return locationManager.getBestProvider(criteria, true);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -34,4 +98,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
