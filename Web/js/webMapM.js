b3a21b68bdadb0f2d7371be566c55fad1ca29b96
Vytenis Slivinskas <slivinskasvytenis@gmail.com>
2015-08-15 17:37:42 +0300

    ištaisytos klaidos

--- Web/js/webMapM.js
+++ Web/js/webMapM.js
@@ -4,10 +4,10 @@ var size = {
 	update : function () {
 		this.width = $( document ).width()/2;
 		this.height = $( document ).height()/2;
-		$( "map1").height() = this.width;
-		$( "map2").height() = this.height;
-		$( "map1").width() = this.width;
-		$( "map2").width() = this.height;
+		$( "map1").height(this.width);
+		$( "map2").height(this.height);
+		$( "map1").width(this.width);
+		$( "map2").width(this.height);
 	}
 }
 
