621c2d7154c3023f837290cfca981494d0957b1e
Alius <alius.petraska@gmail.com>
2015-08-16 10:51:49 +0300

    fix;

--- KibirasInterneto/app/src/main/java/com/example/kibirasinterneto/kibirasinterneto/MainActivity.java
+++ KibirasInterneto/app/src/main/java/com/example/kibirasinterneto/kibirasinterneto/MainActivity.java
@@ -67,7 +67,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
                 view.loadUrl("javascript:changeAdds('photo')");
             }
         });
